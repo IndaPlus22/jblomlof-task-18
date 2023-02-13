f9b4f67b6dc992cf397bc847f01f80c5b58f8e8b
jonathan-blomlof <joning.b@hotmail.com>
2023-02-13 14:37:37 +0100

    Refractor storage system. Make it work as intended.

--- korpus/src/compile_files.rs
+++ korpus/src/compile_files.rs
@@ -1,7 +1,7 @@
 use encoding_rs::{mem::convert_utf8_to_latin1_lossy, WINDOWS_1252};
 use encoding_rs_io::DecodeReaderBytesBuilder;
 use std::{
-    fs::OpenOptions,
+    fs::{create_dir, OpenOptions},
     io::{BufRead, BufReader, Write},
 };
 
@@ -11,29 +11,28 @@ use crate::hash_func::lazy_hash;
 const TOKEN_FILE: &str = "helper_files/token.txt";
 
 /*MAKE SURE THIS DIRECTORY EXISTS */
-const FILE_LOCATION: &str = "helper_files/index_files/";
+//setting to v2 to see if v1 and v2 match, I changed what way we store the shit
+const FILE_LOCATION: &str = "helper_files/index_files_v2/";
 
-/* sett to empty if normal i guess
-    To try and see if .txt files saves storage
-*/
-const FILE_EXTENSION: &str = "";
+const MASTER: &str = "MASTER";
 
 pub fn compile_that_shit() {
-    let mut table = read_token_file();
+    let table = read_token_file();
 
-    for vec in &mut table {
+    for vec in &table {
         create_file_for_hash(vec);
     }
 }
 
 /// Read the token file and retrieve a hash table.
-fn read_token_file() -> Vec<Vec<(String, usize)>> {
+/// Each hash has stores(<amount of chars in longest word>, Vec<(<key>, <value>)>)
+fn read_token_file() -> Vec<(usize, Vec<(String, usize)>)> {
     /*
     Inspired from my task-hash
     */
 
-    //the table stores a (String, Vec) where the string is the word and the vec contains indecies
-    let mut table: Vec<Vec<(String, usize)>> = Vec::new();
+    //the table stores a (String, usize) where the string is the word and the usize is the first instance of the word.
+    let mut table: Vec<(usize, Vec<(String, usize)>)> = Vec::new();
 
     let file = OpenOptions::new().read(true).open(TOKEN_FILE).unwrap();
 
@@ -45,14 +44,14 @@ fn read_token_file() -> Vec<Vec<(String, usize)>> {
     );
     let mut line = String::new();
     let mut prev = String::new();
-    let mut sum = 0;
-    let mut _current_line = buf.read_line(&mut line).unwrap();
-    while _current_line != 0 {
+    let mut sum_of_bytes_read = 0;
+    let mut _current_line_size = buf.read_line(&mut line).unwrap();
+    while _current_line_size != 0 {
         let key = line.split_once(" ").unwrap().0;
         let hash = lazy_hash(key);
         if hash >= table.len() {
             //the hash is larger then available hashes just increase the sizes.
-            table.resize(hash + 1, Vec::new())
+            table.resize(hash + 1, (0, Vec::new()))
             //plus one since we want to be able to index by hash e.g table[hash]
         }
 
@@ -60,60 +59,85 @@ fn read_token_file() -> Vec<Vec<(String, usize)>> {
             // it wasnt last, meaning its new.
 
             // We store the key and how many bytes we need to traverse to find the first instance of the key
-            table[hash].push((key.to_string(), sum));
+            table[hash].1.push((key.to_string(), sum_of_bytes_read));
+            if table[hash].0 < key.len() {
+                table[hash].0 = key.len();
+            }
         }
         prev = key.to_string();
         line = String::new();
-        sum += _current_line;
-        _current_line = buf.read_line(&mut line).unwrap();
+        sum_of_bytes_read += _current_line_size;
+        _current_line_size = buf.read_line(&mut line).unwrap();
     }
     table
 }
 
 #[allow(unused_must_use)]
-fn create_file_for_hash(vec: &mut Vec<(String, usize)>) {
+fn create_file_for_hash((longest_key_len, vec): &(usize, Vec<(String, usize)>)) {
     if vec.len() == 0 {
         return;
     }
     let hash_string = &lazy_hash(&vec[0].0).to_string();
-    let file_location = FILE_LOCATION.to_string();
+    let file_location = FILE_LOCATION.to_string() + hash_string;
+    // create FILE_LOCATION/hash_string/
+    // and file FILE_LOCATION/hash_string/MASTER which contains all keys.
+    create_dir(&file_location);
     let mut file = OpenOptions::new()
         .create(true)
         .write(true)
-        .open(file_location + hash_string + FILE_EXTENSION)
+        .open(file_location + "/" + MASTER)
         .unwrap();
 
-    let (_t1, _t2, sorted_table) = sort_into_tree(vec);
-    let len_of_entry = _t1 + convert_to_u8_but_werid(_t2).len() + 1; //ADDING ONE SINCE WE DO "<key> <byte_passed>" e.g we need to count a blank.
-
-    let amount_of_entries_as_slice = convert_to_u8_but_werid(sorted_table.len());
+    let len_of_entry = *longest_key_len;
+    let mut amount_of_entries_as_slice = convert_to_u8_but_werid(vec.len());
     let len_of_entry_as_slice = convert_to_u8_but_werid(len_of_entry);
+    /*println!(
+        "HASHING: {}, ENTRIES: {}, LENGTH_1: {}",
+        hash_string,
+        vec.len(),
+        *longest_key_len
+    );*/
+
+    // We are just gonna dump everything into amount_of_entries..
     /*Write a header containg <amount of entries> <bytes per entry> */
-    file.write_all(&amount_of_entries_as_slice);
-    file.write_all(&[0]); //blank
-    file.write_all(&len_of_entry_as_slice);
-    file.write_all(&[0]); //blank
-
+    amount_of_entries_as_slice.push(0);
+    amount_of_entries_as_slice.extend_from_slice(&len_of_entry_as_slice);
+    amount_of_entries_as_slice.push(0);
     /*Then for each entrie we write <name____>  //we fill with blankspace.*/
-
+    let mut file_count = 0;
     for entry in vec {
+        // first we write the key in masterfile
+
         // This should work, but if doesn't i blame references.
         let mut clone = entry.0.clone();
         let write_bytes = unsafe { clone.as_bytes_mut() };
         convert_utf8_to_latin1_lossy(entry.0.as_bytes(), write_bytes);
-        file.write_all(write_bytes);
+        amount_of_entries_as_slice.extend_from_slice(write_bytes);
 
-        // write trailing 0 after the key + one 0 + leading 0 for value
-        // so we can read value like 10. By knowing it ends on a specific byte.
-        // but we wont. since i just realized there wont be any trailing 0:s,
-        // Still keeping it tho cuz short code!
-        let _temp = convert_to_u8_but_werid(entry.1);
-        for _ in 0..(len_of_entry - entry.0.len() - _temp.len()) {
-            file.write_all(&[0]);
+        // write trailing 0 after the key
+        for _ in 0..(len_of_entry - entry.0.len()) {
+            amount_of_entries_as_slice.push(0);
         }
-        file.write_all(&_temp);
+
+        //then we write into the value file
+        let mut value_file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(FILE_LOCATION.to_string() + hash_string + "/" + &file_count.to_string())
+            .unwrap();
+        let value = entry.1.to_be_bytes();
+        value_file.write_all(&value[(entry.1.leading_zeros() / 8) as usize..value.len()]);
+        file_count += 1;
     }
+    file.write_all(&amount_of_entries_as_slice);
 }
+/*
+Not needed anymore,
+It was used to store the file as a tree like structure
+But I realized that searching a tree and using binary search is just as fast..., since they are basically the same.
+Both start at median value and then accesses the median for half of the current size and so on.
+
+My claim is backed by SO: https://stackoverflow.com/questions/5968937/binary-search-vs-binary-search-tree
 
 /// Returns the len of the string with the longest name, the biggest usize(value), and a vec containing the tree sorted as we want to write it.
 /// Destroys the passed vector.
@@ -145,18 +169,19 @@ fn sort_into_tree(vec: &mut Vec<(String, usize)>) -> (usize, usize, Vec<(String,
         )
     }
 }
+*/
 
-
-//converts a usize to a vec with u8 such that their sum = the usize
+//converts a usize to a vec with u8 such that they are in base u8::MAX
 fn convert_to_u8_but_werid(value: usize) -> Vec<u8> {
+    
     let mut _vec: Vec<u8> = Vec::new();
     let mut _current_val = 0;
     loop {
         if _current_val + u8::MAX as usize >= value {
             //we can safely just push the rest
             _vec.push((value - _current_val) as u8);
-            return _vec
-        }  else {
+            return _vec;
+        } else {
             _vec.push(u8::MAX);
             _current_val += u8::MAX as usize;
         }
